0d06b9aba417f718beb4072d4d279f908caea10e
Qise Salem <salem109@umn.edu>
2026-01-28 13:07:41 -0600

    Added nativewind and tamagui core

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import "../global.css";
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
